d926adbc12775adc4482fb87e792e8e419e69fa4
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2024-09-13 12:54:31 +0545

    refactor: treat header and footer as components

--- pages/about.php
+++ pages/about.php
@@ -1,4 +1,4 @@
-<?php include('../includes/header.php'); ?>
+<?php include('../components/header.php'); ?>
 
 <head>
     <title>About - Chautari</title>
@@ -85,4 +85,4 @@
 </html>
 
 
-<?php include('../includes/footer.php'); ?>
+<?php include('../components/footer.php'); ?>
